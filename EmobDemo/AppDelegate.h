1cc13704863e6602cc5f9df246eb9de900e5dd99
lorne <280077576@qq.com>
2019-03-07 10:07:24 +0800

    Initial Commit

---
+++ EmobDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmobDemo
+//
+//  Created by lorne on 2019/3/7.
+//  Copyright © 2019 lorne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmobDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmobDemo
+//
+//  Created by lorne on 2019/3/7.
+//  Copyright © 2019 lorne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
